4c32da431b75452bf5c66b83d9ef1a44bd2f06af
rahul <rahulbarnwalonlyu2@gmail.com>
2022-10-04 17:55:07 +0530

    update

--- array_delete.js
+++
@@ -1,9 +0,0 @@
-// ========> Delete Element from an array <=========  
-
-let arr = [67,54,34,90,12];
-let position = 2;
-for(let i = position; i < arr.length-1; i++){
-    arr[i] = arr[i+1];
-}
-arr.length = arr.length-1;
-console.warn(arr);
--- recursion.js
+++
@@ -1,9 +0,0 @@
-function apple(x){
-  console.log(x);
-  if(x<150) {
-    apple(x+1);
-  }
-}
-
-let data = 0;
-apple(data);
