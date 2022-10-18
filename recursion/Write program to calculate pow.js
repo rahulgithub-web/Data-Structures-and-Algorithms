0c17423dde8cf8388801531af7a664d18c28b1c5
Rahul Kumar Barnwal <rahulbarnwalonlyu2@gmail.com>
2022-10-19 03:50:50 +0530

    Update Write program to calculate pow.js

--- recursion/Write program to calculate pow.js
+++ recursion/Write program to calculate pow.js
@@ -12,3 +12,20 @@ console.log(power(num,p));
 
 // Time Complexity: O(n)
 // Auxiliary Space: O(n) n is the size of the recursion stack
+
+
+// -----------------------------------------------------------
+let x = 2.00000, n = 10
+
+var myPow = function(x, n) {
+    if(n===0) return 1
+    n = Math.abs(n)
+    let res = n%2===0 ? myPow(x*x,n/2) : myPow(x*x,(n-1)/2) *x
+    return n < 0 ? 1/res : res
+}
+
+console.log(myPow(x,n))
+
+
+// Time Complexity: O(logn)
+// Auxiliary Space: O(n) n is the size of the recursion stack
